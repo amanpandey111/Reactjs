494e230e7611e79be038c06843ec12933417d952
Aman Pandey <amanpandey7656@gmail.com>
2025-08-11 13:18:00 +0530

    practice_conditional2

--- conditional_rendering/rendering/src/components/ConditionalPractice.jsx
+++ conditional_rendering/rendering/src/components/ConditionalPractice.jsx
@@ -1,10 +1,48 @@
 
 const Item = ({name,isPacked}) => {
+    //! conditionally returning JSX
+    // if(isPacked){
+    //     return <li className="packed">{name} ✅</li>
+    // }else{
+    //     return <li className="unpacked">{name}</li>
+    // }
+
+    //! COnditionally Returning nothing with null
+    // if(isPacked){
+    //     return null
+    // }else{
+    //     return <li className="unpacked">{name}</li>
+    // }
+
+    //! Let's Apply ternary operator( ? : )
+    // return(
+    //     <li className="item" >{ isPacked ? name + '✅' : name }</li>
+    // )
+
+    //! Applying ternary operator with striking through
+    // return(
+    //     <li className="item">
+    //         { isPacked ? (
+    //             <del>{name} + ✅</del>
+    //         ) : name}
+    //     </li>
+    // )
+
+    //! Logical AND operator
+    // return(
+    //     <li className="item">
+    //         {name} {isPacked && <span>✅</span>}
+    //     </li>
+    // )
+
+    //! conditionally assigning jsx to a variable
+    let itemcontent = name
     if(isPacked){
-        return <li className="packed">{name} ✅</li>
-    }else{
-        return <li className="unpacked">{name}</li>
+        itemcontent = <span className="packed">{name} ✅</span>
     }
+    return(
+        <li>{itemcontent}</li>
+    )
 }
 
 const ConditionalPractice = () => {
@@ -12,6 +50,8 @@ const ConditionalPractice = () => {
     <div>
         <Item name="without checked" />
         <Item name="with checked" isPacked={true} />
+        <Item name="Reactjs Tutorial" isPacked={true} />
+        <Item name="Javascript tutorial" />
     </div>
   )
 }
