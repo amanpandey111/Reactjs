dfdfdbf437488fb99fa084962c47dbdf420a1941
amanpandey333 <apandey@infoane.com>
2026-07-09 20:53:51 +0530

    performamce pattern started

---
+++ react-design-pattern/performance-pattern/notes.md
@@ -0,0 +1,2 @@
+## In this We Will only cover the performance pattern in react
+-- The Less component re-render the more the performance
---
+++ react-design-pattern/performance-pattern/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>kfjdj</div>
+    </>
+  )
+}
+
+export default App;
